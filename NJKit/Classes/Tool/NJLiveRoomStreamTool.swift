10a86a218636563e8250b90b356100e0b626babe
胡鹏 <64hp@163.com>
2018-07-17 21:53:10 +0800

    delete

--- NJKit/Classes/Tool/NJLiveRoomStreamTool.swift
+++ NJKit/Classes/Tool/NJLiveRoomStreamTool.swift
@@ -134,8 +134,6 @@ extension NJLiveRoomStreamTool {
         if #available(iOS 11, *) {
             webView?.scrollView.contentInsetAdjustmentBehavior = .never
         }
-        
-        webView?.customUserAgent = "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_13_2) AppleWebKit/604.4.7 (KHTML, like Gecko) Version/11.0.2 Safari/604.4.7"
     }
 }
 
